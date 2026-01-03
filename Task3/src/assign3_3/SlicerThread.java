47b8330d151bcbc2a95155defb5613cbf976c199
roiat <roia1@mail.sapir.ac.il>
2026-01-03 19:22:37 +0200

    added files for Q3 , debugging in progress

---
+++ Task3/src/assign3_3/SlicerThread.java
@@ -0,0 +1,23 @@
+package assign3_3;
+
+public class SlicerThread extends Thread {
+	private final SlicerMachine machine;
+
+    public SlicerThread(SlicerMachine machine) {
+        this.machine = machine;
+        setName("Slicer");
+    }
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                machine.sliceVegetables();
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+    }
+	
+
+}
